98d48ef5654f665539863f1d50e00a91e103ddef
Warren Brown <warrengbrn@gmail.com>
2026-07-06 10:21:10 -0400

    edited vite base url

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig(({ mode }) => {
         "@": path.resolve(__dirname, "./src"),
       },
     },
-    base: env.VITE_BASE,
+    base: '/geopulse-dev',
     test: {
       globals: true,
       css: {
